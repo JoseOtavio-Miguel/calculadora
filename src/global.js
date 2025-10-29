a676fc8fdcb1912e85fee9e868c8b71cf8ac63dc
JoseOtavio-Miguel <=>
2025-10-29 09:05:59 -0300

    Calculator Project in React

---
+++ src/global.js
@@ -0,0 +1,13 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    *, body {
+        margin: 0;
+        padding: 0;
+        background-color: #FAFAFA;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+`
